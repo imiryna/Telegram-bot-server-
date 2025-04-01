544aabbbe7fb2ace45230a84e54a7b7ce0872403
Iryna Burlaka <i.am.blinova@gmail.com>
2025-04-01 13:52:39 +0200

    logic of bot

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import TelegramBot from "node-telegram-bot-api";
+
+dotenv.config({});
+
+const app = express();
+
+console.log(process.env);
+
+//== Telegram bot =============
+
+const token = process.env.OT_API_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  if (messageText === "/start") {
+    bot.sendMessage(chatId, "Welcome to the bot!");
+  }
+}); // Process the incoming message here
