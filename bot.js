b26d5819a4fbc5fba598cf7fab7125c0cd179939
Iryna Burlaka <i.am.blinova@gmail.com>
2025-04-01 14:29:11 +0200

    bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,16 @@
-const TelegramBot = require("node-telegram-bot-api");
+import TelegramBot from "node-telegram-bot-api";
+
+//== Telegram bot =============
+
+const token = process.env.BOT_API_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  if (messageText === "/start") {
+    bot.sendMessage(chatId, "Welcome to the bot!");
+  }
+}); // Process the incoming message here
